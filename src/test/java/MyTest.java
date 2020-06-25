f89cb1f213f14de475bf64527ea910aaa2551b5d
XienX <1324548879@qq.com>
2020-06-25 10:46:52 +0800

    test

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyTest {
+
+    @Test
+    public void TodoTestInput1 () {
+        ToDoList list = new ToDoList();
+
+        /*
+        *中间调添加、删除、改状态的方法
+        */
+
+        String result = list.toString;
+        Assert.assertEquals("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", result);
+    }
+}
