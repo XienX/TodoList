cce8adbe4ed9f0382fc9055c33f4d62178122067
yue-1998 <1012336581@qq.com>
2020-06-27 18:55:18 +0800

    finish todoList

--- src/main/java/TodoList.java
+++ src/main/java/TodoList.java
@@ -1,2 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TodoList {
+    List<Todo> list = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        StringBuilder allTodo = new StringBuilder();
+        for (Todo todo: list){
+            allTodo.append('\n').append(todo.toString());
+        }
+        return "TodoList{" +
+                allTodo +
+                "}\n";
+    }
+
+    public int add(Todo todo){
+        int myId;
+        if (list.size() == 0) {
+            myId = 1;
+        }
+        else {
+            myId = list.get(list.size()-1).getId()+1;
+        }
+
+        todo.setId(myId);
+        list.add(todo);
+        return myId;
+    }
+
+    public int delete(Todo todo){
+        int id = todo.getId();
+        for (Todo todo1 : list) {
+            if (todo1.getId() == id) {
+                list.remove(todo1);
+                return id;
+            }
+        }
+
+        return -1;
+    }
+    public int completed(Todo todo) {
+        int id = todo.getId();
+        for (Todo todo1 : list) {
+            if (todo1.getId() == id) {
+                todo1.setCompleted(true);
+                return id;
+            }
+        }
+
+        return -1;
+    }
 }
